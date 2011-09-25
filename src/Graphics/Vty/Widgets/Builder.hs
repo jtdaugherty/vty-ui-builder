9042c7ba9ceee96ec534420f46a494ae239633cb
Jonathan Daugherty <jtd@galois.com>
2011-09-25 02:53:41 -0700

    builder-test: factor out main source generation routine into a module

---
+++ src/Graphics/Vty/Widgets/Builder.hs
@@ -0,0 +1,39 @@
+module Graphics.Vty.Widgets.Builder
+    ( generateModuleSource
+    )
+where
+
+import Control.Monad.State
+import Text.PrettyPrint.HughesPJ
+
+import Text.XML.HaXml.Parse hiding (doctypedecl)
+import Text.XML.HaXml.Validate
+import Text.XML.HaXml.Types
+
+import Graphics.Vty.Widgets.Builder.Types
+import Graphics.Vty.Widgets.Builder.GenLib
+import Graphics.Vty.Widgets.Builder.Handlers
+import Graphics.Vty.Widgets.Builder.DTDGenerator
+
+generateModuleSource :: FilePath
+                     -> FilePath
+                     -> [(String, ElementHandler a)]
+                     -> IO String
+generateModuleSource inputXmlPath dtdPath extraHandlers = do
+  masterDTD <- generateMasterDTD (elementHandlers ++ extraHandlers) dtdPath
+  dtd <- case dtdParse' "<generated>" masterDTD of
+           Right (Just dtd) -> return dtd
+           Right Nothing -> error "No DTD found in generated DTD text!"
+           Left e -> error $ "Error parsing generated DTD: " ++ e
+
+  xmlContents <- readFile inputXmlPath
+  case xmlParse' inputXmlPath xmlContents of
+    Left e -> error $ "Error parsing input XML " ++ (show inputXmlPath) ++ ": " ++ e
+    Right (Document _ _ e _) -> do
+         case partialValidate dtd e of
+           [] -> do
+             let (_, st') = runState (gen e "root") (GenState 0 empty elementHandlers)
+             return $ render $ genDoc st'
+           es -> do
+             mapM_ putStrLn es
+             error $ "Error validating " ++ (show inputXmlPath)
--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,11 @@
 module Main where
 
-import Text.XML.HaXml.Parse hiding (doctypedecl)
-import Text.XML.HaXml.Validate
-import Text.XML.HaXml.Types
-
 import System
 import System.FilePath ((</>))
 import Control.Monad
-import Control.Monad.State
 
 import Paths_vty_ui_builder
-
-import Text.PrettyPrint.HughesPJ
-
-import Graphics.Vty.Widgets.Builder.DTDGenerator
-import Graphics.Vty.Widgets.Builder.Types
-import Graphics.Vty.Widgets.Builder.GenLib
-import Graphics.Vty.Widgets.Builder.Handlers
+import Graphics.Vty.Widgets.Builder
 
 usage :: String
 usage = "builder-test <XML filename>"
@@ -31,19 +20,4 @@ main = do
   dataDir <- getDataDir
   let dtdPath = dataDir </> "dtd"
 
-  masterDTD <- generateMasterDTD elementHandlers dtdPath
-  dtd <- case dtdParse' "<generated>" masterDTD of
-           Right (Just dtd) -> return dtd
-           Right Nothing -> error "No DTD found in generated DTD text!"
-           Left e -> error $ "Error parsing generated DTD: " ++ e
-
-  xmlContents <- readFile xmlFilename
-  case xmlParse' xmlFilename xmlContents of
-    Left e -> putStrLn e
-
-    Right (Document _ _ e _) -> do
-         case partialValidate dtd e of
-           [] -> do
-             let (_, st') = runState (gen e "root") (GenState 0 empty elementHandlers)
-             putStrLn $ render $ genDoc st'
-           es -> mapM_ putStrLn es
+  putStrLn =<< generateModuleSource xmlFilename dtdPath []
